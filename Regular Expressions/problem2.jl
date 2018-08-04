c0619d3131cc4408b4c826c05e0f6ec65ac7cad3
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-08-04 21:26:56 +0800

    2018 Updates

---
+++ Regular Expressions/problem2.jl
@@ -0,0 +1,3 @@
+#Problem 2: Write a program that matches email address.
+println(ismatch(r"^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$", "mark@yahoo.com"))
+println(ismatch(r"^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$", "mark-yahoo.com"))
