409d45e952e114fbcd282807c7f2a8c0cd4fe659
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-08-06 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/problem14.jl
@@ -0,0 +1,3 @@
+#Problem 14: Write a program that recognizes valid hex color value using Regular Expression.
+println(ismatch(r"^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$", "192.168.1.1"))
+println(ismatch(r"^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$", "1.1.1.1.1"))
