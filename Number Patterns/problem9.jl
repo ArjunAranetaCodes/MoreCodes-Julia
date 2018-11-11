654d5ad2fa17298450d39e64ecf68ea630403e1e
Arjun Araneta <arjunaraneta@gmail.com>
2018-11-12 06:14:24 +0800

    2018 Updates

---
+++ Number Patterns/problem9.jl
@@ -0,0 +1,22 @@
+#=
+Problem 9: Write a program to print the number pattern of ones and zeros using loop.
+11011
+11011
+00000
+11011
+11011
+=#
+row = 4
+col = 4
+for y in 0:row 
+ for x in 0:col 
+  if(col / 2 == x || row / 2 == y) then
+   print("0")
+  elseif((col % 2 == 0 && (col / 2) == x) || (row % 2 == 0 && (row / 2) == y))
+   print("0")
+  else
+   print("1")
+  end		
+ end
+ print("\n")
+end
